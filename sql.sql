c6042d9e042e6369cc5c89cb90ce1364ac0b31f1
Christian Goldoni <christian.goldoni@gmail.com>
2019-11-22 14:29:49 -0500

    added second part

--- sql.sql
+++ sql.sql
@@ -32,4 +32,41 @@ WITH order_per_month AS(
             unit_sold_per_month - previous_month AS DIFFERENCE
         FROM previous_month_order;
 
-  
+-- second part, filling in the missing months from the results (the months where there were no sales for that product)
+
+WITH order_per_month AS(
+    SELECT
+        product_id,
+        to_char(order_date, 'YYYY-MM') AS year_month_order,
+        SUM(quantity) AS unit_sold_per_month
+    FROM orders
+    JOIN order_details USING(order_id)
+    GROUP BY product_id, year_month_order
+    ORDER BY product_id, year_month_order DESC    
+    ), product_all_date AS( 
+        SELECT 
+            product_id,
+            to_char(dates, 'YYYY-MM') as year_month_order
+        FROM generate_series((SELECT min(order_date) FROM orders), 
+                        (SELECT max(order_date) FROM orders), '1 Month') as dates 
+        CROSS JOIN (SELECT DISTINCT product_id FROM products) as products_list
+        ORDER BY product_id, dates DESC
+        ), order_per_month_all_date AS(
+            SELECT
+                pd.product_id,
+                pd.year_month_order,
+                COALESCE(unit_sold_per_month, 0) AS unit_sold_per_month
+            FROM product_all_date pd
+            LEFT JOIN order_per_month om ON pd.product_id = om.product_id AND 
+                                                pd.year_month_order = om.year_month_order
+            ORDER BY pd.product_id, pd.year_month_order DESC
+            ), previous_month_order AS(
+                    SELECT
+                        *,
+                        COALESCE(LEAD(unit_sold_per_month, 1) OVER(PARTITION BY product_id ORDER BY year_month_order DESC), 0) 
+                            AS previous_month
+                    FROM order_per_month_all_date
+                )   SELECT
+                        *,
+                        unit_sold_per_month - previous_month AS difference 
+                    FROM previous_month_order;
